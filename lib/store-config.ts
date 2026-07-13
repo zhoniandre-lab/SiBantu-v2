e64cfb22ab1beda803f5447c04ff746e2edbb9c7
zhoniandre-lab <zhoniandre@gmail.com>
2026-07-13 21:41:17 +0700

    Add STORE_CONFIG and adminWhatsAppUrl function

---
+++ lib/store-config.ts
@@ -0,0 +1,12 @@
+export const STORE_CONFIG = {
+  name: 'SiBantu',
+  adminPhoneDisplay: '0852-7313-9959',
+  adminPhoneInternational: '6285273139959',
+  pickupName: 'Pasar Inpres',
+  pickupAddress: 'Pasar Inpres, Desa Kepala Pasar, Kecamatan Kaur Selatan',
+  deliveryArea: 'Kaur Selatan dan sekitarnya',
+};
+
+export function adminWhatsAppUrl(message: string) {
+  return `https://wa.me/${STORE_CONFIG.adminPhoneInternational}?text=${encodeURIComponent(message)}`;
+}
