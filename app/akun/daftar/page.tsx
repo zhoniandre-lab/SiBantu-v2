29a0ba51516b111307095335846fca3d4249553d
zhoniandre-lab <zhoniandre@gmail.com>
2026-07-17 02:23:13 +0700

    Add CustomerRegistration component to DaftarPembeli

---
+++ app/akun/daftar/page.tsx
@@ -0,0 +1,2 @@
+import CustomerRegistration from '@/components/account/CustomerRegistration';
+export default function DaftarPembeli(){return <section className="account-auth-page"><CustomerRegistration/></section>}
